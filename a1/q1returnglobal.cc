8f463e74148e2253179f689fac67e717a052f2f2
j76xiao <j76xiao@uwaterloo.ca>
2025-01-14 22:19:27 -0500

    Update formate of q1

--- a1/q1returnglobal.cc
+++ a1/q1returnglobal.cc
@@ -1,7 +1,8 @@
+#include <unistd.h>
+
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include <unistd.h>
 
 using namespace std;
 
@@ -12,8 +13,8 @@ bool ex3_flag = false;
 intmax_t eperiod = 10000;
 int randcnt = 0;
 int Rand() {
-  randcnt += 1;
-  return rand();
+	randcnt += 1;
+	return rand();
 }
 
 short int ex1_code;
@@ -21,110 +22,105 @@ int ex2_code;
 long int ex3_code;
 
 double rtn1(double i) {
-  if (Rand() % eperiod == 0) {
-    ex1_flag = true;
-    ex1_code = (short int)Rand();
-    return 0.0;
-  }
-  return i + Rand();
+	if (Rand() % eperiod == 0) {
+		ex1_flag = true;
+		ex1_code = (short int)Rand();
+		return 0.0;
+	}
+	return i + Rand();
 }
 
 double rtn2(double i) {
-  if (Rand() % eperiod == 0) {
-    ex2_flag = true;
-    ex2_code = Rand();
-    return 0.0;
-  }
-  double result = rtn1(i);
-  if (ex1_flag)
-    return 0.0;
-  return result + Rand();
+	if (Rand() % eperiod == 0) {
+		ex2_flag = true;
+		ex2_code = Rand();
+		return 0.0;
+	}
+	double result = rtn1(i);
+	if (ex1_flag) return 0.0;
+	return result + Rand();
 }
 
 double rtn3(double i) {
-  if (Rand() % eperiod == 0) {
-    ex3_flag = true;
-    ex3_code = Rand();
-    return 0.0;
-  }
-  double result = rtn2(i);
-  if (ex1_flag || ex2_flag)
-    return 0.0;
-  return result + Rand();
+	if (Rand() % eperiod == 0) {
+		ex3_flag = true;
+		ex3_code = Rand();
+		return 0.0;
+	}
+	double result = rtn2(i);
+	if (ex1_flag || ex2_flag) return 0.0;
+	return result + Rand();
 }
 
-static intmax_t convert(const char *str) { // convert C string to integer
-  char *endptr;
-  errno = 0;                                // reset
-  intmax_t val = strtoll(str, &endptr, 10); // attempt conversion
-  if (errno == ERANGE)
-    throw std::out_of_range("");
-  if (endptr == str || // conversion failed, no characters generated
-      *endptr != '\0')
-    throw std::invalid_argument(""); // not at end of str ?
-  return val;
-} // convert
+static intmax_t convert(const char *str) {	// convert C string to integer
+	char *endptr;
+	errno = 0;								   // reset
+	intmax_t val = strtoll(str, &endptr, 10);  // attempt conversion
+	if (errno == ERANGE) throw std::out_of_range("");
+	if (endptr == str ||  // conversion failed, no characters generated
+		*endptr != '\0')
+		throw std::invalid_argument("");  // not at end of str ?
+	return val;
+}  // convert
 
 int main(int argc, char *argv[]) {
-  intmax_t times = 100000000, seed = getpid(); // default values
-  struct cmd_error {};
+	intmax_t times = 100000000, seed = getpid();  // default values
+	struct cmd_error {};
 
-  try {
-    switch (argc) {
-    case 4:
-      if (strcmp(argv[3], "d") != 0) { // default ?
-        seed = convert(argv[3]);
-        if (seed <= 0)
-          throw cmd_error();
-      }
-    case 3:
-      if (strcmp(argv[2], "d") != 0) { // default ?
-        eperiod = convert(argv[2]);
-        if (eperiod <= 0)
-          throw cmd_error();
-      }
-    case 2:
-      if (strcmp(argv[1], "d") != 0) { // default ?
-        times = convert(argv[1]);
-        if (times <= 0)
-          throw cmd_error();
-      }
-    case 1:
-      break; // use all defaults
-    default:
-      throw cmd_error();
-    } // switch
-  } catch (...) {
-    cerr << "Usage: " << argv[0]
-         << " [ times > 0 | d [ eperiod > 0 | d [ seed > 0 | d ] ] ]" << endl;
-    exit(EXIT_FAILURE);
-  } // try
-  srand(seed);
+	try {
+		switch (argc) {
+			case 4:
+				if (strcmp(argv[3], "d") != 0) {  // default ?
+					seed = convert(argv[3]);
+					if (seed <= 0) throw cmd_error();
+				}
+			case 3:
+				if (strcmp(argv[2], "d") != 0) {  // default ?
+					eperiod = convert(argv[2]);
+					if (eperiod <= 0) throw cmd_error();
+				}
+			case 2:
+				if (strcmp(argv[1], "d") != 0) {  // default ?
+					times = convert(argv[1]);
+					if (times <= 0) throw cmd_error();
+				}
+			case 1:
+				break;	// use all defaults
+			default:
+				throw cmd_error();
+		}  // switch
+	} catch (...) {
+		cerr << "Usage: " << argv[0]
+			 << " [ times > 0 | d [ eperiod > 0 | d [ seed > 0 | d ] ] ]"
+			 << endl;
+		exit(EXIT_FAILURE);
+	}  // try
+	srand(seed);
 
-  double rv = 0.0;
-  int ev1 = 0, ev2 = 0, ev3 = 0;
-  int rc = 0, ec1 = 0, ec2 = 0, ec3 = 0;
+	double rv = 0.0;
+	int ev1 = 0, ev2 = 0, ev3 = 0;
+	int rc = 0, ec1 = 0, ec2 = 0, ec3 = 0;
 
-  for (int i = 0; i < times; i += 1) {
-    ex1_flag = ex2_flag = ex3_flag = false;
-    double result = rtn3(i);
-    if (ex1_flag) {
-      ev1 += ex1_code;
-      ec1++;
-    } else if (ex2_flag) {
-      ev2 += ex2_code;
-      ec2++;
-    } else if (ex3_flag) {
-      ev3 += ex3_code;
-      ec3++;
-    } else {
-      rv += result;
-      rc++;
-    }
-  }
-  cout << "randcnt " << randcnt << endl;
-  cout << "normal result " << rv << " exception results " << ev1 << ' ' << ev2
-       << ' ' << ev3 << endl;
-  cout << "calls " << rc << " exceptions " << ec1 << ' ' << ec2 << ' ' << ec3
-       << endl;
+	for (int i = 0; i < times; i += 1) {
+		ex1_flag = ex2_flag = ex3_flag = false;
+		double result = rtn3(i);
+		if (ex1_flag) {
+			ev1 += ex1_code;
+			ec1++;
+		} else if (ex2_flag) {
+			ev2 += ex2_code;
+			ec2++;
+		} else if (ex3_flag) {
+			ev3 += ex3_code;
+			ec3++;
+		} else {
+			rv += result;
+			rc++;
+		}
+	}
+	cout << "randcnt " << randcnt << endl;
+	cout << "normal result " << rv << " exception results " << ev1 << ' ' << ev2
+		 << ' ' << ev3 << endl;
+	cout << "calls " << rc << " exceptions " << ec1 << ' ' << ec2 << ' ' << ec3
+		 << endl;
 }
--- a1/q1returntypec.c
+++ a1/q1returntypec.c
@@ -7,122 +7,120 @@
 typedef enum { RESULT_DOUBLE, RESULT_EX1, RESULT_EX2, RESULT_EX3 } ResultType;
 
 typedef struct {
-  ResultType type;
-  union {
-    double value;
-    short int ex1_code;
-    int ex2_code;
-    long int ex3_code;
-  };
+	ResultType type;
+	union {
+		double value;
+		short int ex1_code;
+		int ex2_code;
+		long int ex3_code;
+	};
 } Result;
 
 intmax_t eperiod = 10000;
 int randcnt = 0;
 int Rand() {
-  randcnt += 1;
-  return rand();
+	randcnt += 1;
+	return rand();
 }
 
 Result rtn1(double i) {
-  if (Rand() % eperiod == 0) {
-    Result result = {.type = RESULT_EX1, .ex1_code = (short int)Rand()};
-    return result;
-  }
-  Result result = {.type = RESULT_DOUBLE, .value = i + Rand()};
-  return result;
+	if (Rand() % eperiod == 0) {
+		Result result = {.type = RESULT_EX1, .ex1_code = (short int)Rand()};
+		return result;
+	}
+	Result result = {.type = RESULT_DOUBLE, .value = i + Rand()};
+	return result;
 }
 
 Result rtn2(double i) {
-  if (Rand() % eperiod == 0) {
-    Result result = {.type = RESULT_EX2, .ex2_code = Rand()};
-    return result;
-  }
-  Result res1 = rtn1(i);
-  if (res1.type != RESULT_DOUBLE)
-    return res1;
-  Result result = {.type = RESULT_DOUBLE, .value = res1.value + Rand()};
-  return result;
+	if (Rand() % eperiod == 0) {
+		Result result = {.type = RESULT_EX2, .ex2_code = Rand()};
+		return result;
+	}
+	Result res1 = rtn1(i);
+	if (res1.type != RESULT_DOUBLE) return res1;
+	Result result = {.type = RESULT_DOUBLE, .value = res1.value + Rand()};
+	return result;
 }
 
 Result rtn3(double i) {
-  if (Rand() % eperiod == 0) {
-    Result result = {.type = RESULT_EX3, .ex3_code = Rand()};
-    return result;
-  }
-  Result res2 = rtn2(i);
-  if (res2.type != RESULT_DOUBLE)
-    return res2;
-  Result result = {.type = RESULT_DOUBLE, .value = res2.value + Rand()};
-  return result;
+	if (Rand() % eperiod == 0) {
+		Result result = {.type = RESULT_EX3, .ex3_code = Rand()};
+		return result;
+	}
+	Result res2 = rtn2(i);
+	if (res2.type != RESULT_DOUBLE) return res2;
+	Result result = {.type = RESULT_DOUBLE, .value = res2.value + Rand()};
+	return result;
 }
 
 int main(int argc, char *argv[]) {
-  intmax_t times = 100000000, seed = getpid();
-  int arg_error = 0;
+	intmax_t times = 100000000, seed = getpid();
+	int arg_error = 0;
 
-  // Multi-level exit for command-line argument parsing
-  do {
-    if (argc > 1) {
-      times = strtoll(argv[1], NULL, 10);
-      if (times <= 0) {
-        arg_error = 1;
-        break;
-      }
-    }
-    if (argc > 2) {
-      eperiod = strtoll(argv[2], NULL, 10);
-      if (eperiod <= 0) {
-        arg_error = 1;
-        break;
-      }
-    }
-    if (argc > 3) {
-      seed = strtoll(argv[3], NULL, 10);
-      if (seed <= 0) {
-        arg_error = 1;
-        break;
-      }
-    }
-  } while (0);
+	// Multi-level exit for command-line argument parsing
+	do {
+		if (argc > 1) {
+			times = strtoll(argv[1], NULL, 10);
+			if (times <= 0) {
+				arg_error = 1;
+				break;
+			}
+		}
+		if (argc > 2) {
+			eperiod = strtoll(argv[2], NULL, 10);
+			if (eperiod <= 0) {
+				arg_error = 1;
+				break;
+			}
+		}
+		if (argc > 3) {
+			seed = strtoll(argv[3], NULL, 10);
+			if (seed <= 0) {
+				arg_error = 1;
+				break;
+			}
+		}
+	} while (0);
 
-  if (arg_error) {
-    fprintf(stderr,
-            "Usage: %s [times > 0 | d [eperiod > 0 | d [seed > 0 | d]]]\n",
-            argv[0]);
-    exit(EXIT_FAILURE);
-  }
+	if (arg_error) {
+		fprintf(stderr,
+				"Usage: %s [times > 0 | d [eperiod > 0 | d [seed > 0 | d]]]\n",
+				argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-  srand(seed);
+	srand(seed);
 
-  double rv = 0.0;
-  int ev1 = 0, ev2 = 0, ev3 = 0;
-  int rc = 0, ec1 = 0, ec2 = 0, ec3 = 0;
+	double rv = 0.0;
+	int ev1 = 0, ev2 = 0, ev3 = 0;
+	int rc = 0, ec1 = 0, ec2 = 0, ec3 = 0;
 
-  for (int i = 0; i < times; i++) {
-    Result res = rtn3((double)i);
-    switch (res.type) {
-    case RESULT_DOUBLE:
-      rv += res.value;
-      rc++;
-      break;
-    case RESULT_EX1:
-      ev1 += res.ex1_code;
-      ec1++;
-      break;
-    case RESULT_EX2:
-      ev2 += res.ex2_code;
-      ec2++;
-      break;
-    case RESULT_EX3:
-      ev3 += res.ex3_code;
-      ec3++;
-      break;
-    }
-  }
+	for (int i = 0; i < times; i++) {
+		Result res = rtn3((double)i);
+		switch (res.type) {
+			case RESULT_DOUBLE:
+				rv += res.value;
+				rc++;
+				break;
+			case RESULT_EX1:
+				ev1 += res.ex1_code;
+				ec1++;
+				break;
+			case RESULT_EX2:
+				ev2 += res.ex2_code;
+				ec2++;
+				break;
+			case RESULT_EX3:
+				ev3 += res.ex3_code;
+				ec3++;
+				break;
+		}
+	}
 
-  printf("randcnt %d\n", randcnt);
-  printf("normal result %g exception results %d %d %d\n", rv, ev1, ev2, ev3);
-  printf("calls %d exceptions %d %d %d\n", rc, ec1, ec2, ec3);
+	printf("randcnt %d\n", randcnt);
+	printf("normal result %g exception results %d %d %d\n", rv, ev1, ev2, ev3);
+	printf("calls %d exceptions %d %d %d\n", rc, ec1, ec2, ec3);
 
-  return 0;
+	return 0;
 }
