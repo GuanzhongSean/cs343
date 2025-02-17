ff5147b7aa18f0adf6d3a017f30e6e34bca6f5c8
Sean Xiao <j76xiao@uwaterloo.ca>
2025-02-16 21:57:34 -0500

    pre

--- a3/q2.cc
+++
@@ -1,4 +0,0 @@
-template<typename T>
-void quicksort( T values[], unsigned int low, unsigned int high, unsigned int depth );
-
-pivot = array[low + ( high - low ) / 2];
---
+++ a3/q2quicksort.h
@@ -0,0 +1,8 @@
+#ifndef Q2QUICKSORT_H
+#define Q2QUICKSORT_H
+
+template <typename T>
+void quicksort(T values[], unsigned int low, unsigned int high,
+			   unsigned int depth);
+
+#endif
